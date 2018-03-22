c4fb556111d94b643b750966c813b38bf921deea
James Hall-Treworgy <jameshall-treworgy@Jamess-MacBook-Pro.local>
2018-03-21 23:39:10 -0400

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Pipeline Operator
+This projects allows one to use the Pipeline Operator
+
+## How To Run: 
+
+#### Download Dependencies 
+`npm install`
+
+#### Compile Code
+`npm start`
+
+## Learn More
+Pipeline Operator - https://github.com/tc39/proposal-pipeline-operator
---
+++ pipe.js
@@ -0,0 +1,26 @@
+const double = (n) => n * 2;
+const increment = (n) => n + 1;
+
+// without pipeline operator
+double(increment(5)); // 12
+
+// with pipeline operator
+const result = 5 
+  |> increment 
+  |> double 
+
+console.log(result); // 12
+
+const capitalize = str => str[0].toUpperCase() + str.substring(1);
+const splitOnSpaces = str => str.trim().split(' ');
+const getLastOfArr = arr => arr[arr.length - 1];
+
+const lastName = 'albert einstein'
+  |> splitOnSpaces
+  |> getLastOfArr
+  |> capitalize
+
+console.log(lastName);
+
+// console.log(capitalize(getLastOfArr(splitOnSpaces('albert einstein'))));
+ 
